5b20c12ef5f4785a9d16cd47075f794540f1d2a6
4rnv <arnavsdandekar@gmail.com>
2025-09-12 10:53:23 +0530

    2

--- internal/gui/gui.go
+++ internal/gui/gui.go
@@ -13,6 +13,37 @@ const HEIGHT = 600
 const HSTEP, VSTEP = 12, 18
 const SCROLL_STEP = 50
 
+type Browser struct {
+	Window       string // replace with fyne window
+	Width        int
+	Height       int
+	Canvas       string // replace with fyne canvas
+	Text         string
+	Display_List []string
+}
+
+func Special_Page() {
+	//draw special page here
+}
+
+func (Browser) Load(url yurl.URL, browser Browser) {
+	content, err := url.Request(0)
+	if err != nil {
+		Special_Page()
+		return
+	}
+	browser.Text = yurl.Lex(content)
+	browser.Display_List = append(browser.Display_List, Layout(browser.Text, browser.Width))
+	//Draw()
+}
+
+func Layout(text string, width int) string {
+	return ""
+}
+
+func Draw() {
+}
+
 // WILL BE CHANGED TO FUNC RUN AFTER TESTING
 func main() {
 	if len(os.Args) < 2 {
